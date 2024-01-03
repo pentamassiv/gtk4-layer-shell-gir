78c47d8e89faf26b116474940d3539ef1e9392b7
pentamassiv <pentamassiv@posteo.de>
2024-01-03 18:35:57 +0100

    Change from subtree to submodule

--- gir/src/analysis/general.rs
+++
@@ -1,8 +0,0 @@
-use crate::{config::gobjects::*, library::*};
-
-#[derive(Debug, Clone)]
-pub struct StatusedTypeId {
-    pub type_id: TypeId,
-    pub name: String,
-    pub status: GStatus,
-}
--- gir/src/library_preprocessing.rs
+++
@@ -1,7 +0,0 @@
-use crate::{config::WorkMode, library::*};
-
-impl Library {
-    pub fn preprocessing(&mut self, work_mode: WorkMode) {
-        self.add_glib_priority(work_mode);
-    }
-}
