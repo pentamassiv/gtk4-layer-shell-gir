b0d7a785a689fda06851308e35a607ff1e0fc4b0
pentamassiv <91755244+pentamassiv@users.noreply.github.com>
2025-09-01 12:49:59 +0000

    Automatically updated the version of gir or gir-files

--- gtk4-layer-shell/src/manual.rs
+++ gtk4-layer-shell/src/manual.rs
@@ -2,6 +2,12 @@ use glib::object::IsA;
 use glib::translate::*;
 use gtk4_layer_shell_sys as ffi;
 
+/// ## `window`
+/// A layer surface.
+///
+/// # Returns
+///
+/// The underlying layer surface Wayland object
 #[doc(alias = "gtk_layer_get_zwlr_layer_surface_v1")]
 #[doc(alias = "get_zwlr_layer_surface_v1")]
 pub fn zwlr_layer_surface_v1(
